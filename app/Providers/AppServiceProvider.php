65e3ea9190988d1ab018a78489bbebb95ca16b2f
dmdboi <hello@maxdiamond.co.uk>
2024-12-21 12:23:37 +0000

    WIP: experimental JSON themes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Facades\FilamentCMS;
+use App\Services\FilamentCMSThemes;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
         $theme = config('filament-cms.theme');
 
         // Get the theme's sections from Views/Themes/{theme}.php
-        $sections = require app_path("Views/Themes/{$theme}.php");
+        $sections = FilamentCMSThemes::parseSectionJSON(file_get_contents(app_path("Views/Themes/{$theme}.json")));
 
         // Register the theme's sections
         FilamentCMS::themes()->register($sections);
--- app/Services/FilamentCMSThemes.php
+++ app/Services/FilamentCMSThemes.php
@@ -3,6 +3,10 @@
 namespace App\Services;
 
 use App\Services\Contracts\Section;
+use Exception;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Support\Collection;
 
 class FilamentCMSThemes
@@ -26,4 +30,71 @@ class FilamentCMSThemes
     {
         return collect(self::$sections);
     }
+
+    public static function parseSectionJSON(string $json): array
+    {
+
+        $jsonArray = json_decode($json, true);
+
+        // If the input is a single section, wrap it in an array
+        if (isset($jsonArray['name'])) {
+            $jsonArray = [$jsonArray];
+        }
+
+        return array_map(function ($json) {
+            return Section::make($json['name'])
+                ->label($json['label'] ?? '')
+                ->view($json['view'] ?? '')
+                ->form(
+                    self::resolveFormArrayToObjects($json['form'] ?? [])
+                );
+        }, $jsonArray);
+    }
+
+    public static function resolveFormArrayToObjects(array $form): array
+    {
+        return array_map(function ($field) {
+            switch ($field['type']) {
+                case 'TextInput':
+                    $textInput = TextInput::make($field['name']);
+
+                    if (!empty($field['label'])) {
+                        $textInput = $textInput->label($field['label']);
+                    }
+
+                    if (!empty($field['required']) && $field['required'] === true) {
+                        $textInput = $textInput->required();
+                    }
+
+                    return $textInput;
+
+                case 'Repeater':
+                    $repeater = Repeater::make($field['name'])->label($field['label']);
+
+                    if (!empty($field['collapsible']) && $field['collapsible'] === true) {
+                        $repeater = $repeater->collapsible();
+                    }
+
+                    if (!empty($field['schema'])) {
+                        $repeater = $repeater->schema(resolveFormArrayToObjects($field['schema']));
+                    }
+
+                    return $repeater;
+
+                case 'Select':
+                    $select = Select::make($field['name'])->label($field['label']);
+
+                    if (!empty($field['options'])) {
+                        $select = $select->options($field['options']);
+                    }
+
+                    return $select;
+
+                default:
+                    throw new Exception("Unsupported field type: {$field['type']}");
+            }
+        }, $form);
+    }
+
+
 }
